6780086404f95db0f405b64cd168caa7d7a23c81
Gabytto <gabyttox@gmail.com>
2025-05-20 11:22:04 -0300

    ultimo

--- menu.py
+++ menu.py
@@ -81,7 +81,18 @@ while activo:
                 print('-'*46)
 
     #3. BUSCAR UN PRODUCTO
-
+    elif entrada == 3:
+        print('-'*46)
+        registro = input('Ingresa el nombre del producto a buscar: ')
+        for i, producto in enumerate(productos):
+            if registro in producto[0]:
+                print('-'*46)
+                print(f'Producto encontrado: ')
+                print(f'Producto {i + 1}: ')
+                print(f'Nombre del producto: {producto[0].title()}')
+                print(f'Categoría: {producto[1]}')
+                print(f'Precio: {producto[2]}')
+                print('-'*46)
      
     #4.ACTUALIZAR UN PRODUCTO
 
